d8e03635c75faeeabef8d728cedde48680739d4b
unknown <louilou008@gmail.com>
2020-05-10 17:45:30 +0200

    [Add] open file dialog and checks

--- PE Dissector/MainWindow.cpp
+++ PE Dissector/MainWindow.cpp
@@ -5,8 +5,63 @@ MainWindow::MainWindow(QWidget *parent)
 {
 	ui.setupUi(this);
 
-	ui.treeWidget->setColumnCount(0);
 	statusBarLabel = new QLabel(QString("No file loaded"), ui.statusBar);
 	ui.statusBar->addWidget(statusBarLabel);
 
+	ui.headerTree->setColumnCount(0);
+}
+
+void MainWindow::headerTree_selectionChanged()
+{
+	qDebug() << "toto";
+}
+
+void MainWindow::actionOpen_File_triggered()
+{
+	qDebug() << "open file";
+	QStringList fileList = QFileDialog::getOpenFileNames(this, QString("Select a PE file to dissect"), QString(), 
+		QString("All files (*.*);;All PE files (*.exe *.dll *.sys *.drv *.ocx *.cpl *.scr);;Exe files (*.exe);;Dll files (*.dll);;System files (*.sys *.drv);;ActiveX control files (*.ocx);;Control panel files (*.cpl);;Screensaver files(*.scr)"),
+		&QString("All PE files (*.exe *.dll *.sys *.drv *.ocx *.cpl *.scr)"));
+	for (QString filename : fileList)
+	{
+		qDebug() << "file : ";
+		qDebug() << filename;
+		addFile(filename);
+	}
+}
+
+bool MainWindow::addFile(QString filename)
+{
+	statusBarLabel->setText(QString("Loading file ") + QFileInfo(filename).fileName() + QString("..."));
+	HANDLE hFile = CreateFileA(filename.toLocal8Bit().data(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		statusBarLabel->setText(QString("Failed to open ") + QFileInfo(filename).fileName() + QString("!"));
+		return false;
+	}
+
+	if (!isFileExecutable(hFile))
+	{
+		statusBarLabel->setText(QFileInfo(filename).fileName() + QString(" is not a PE file"));
+		return false;
+	}
+
+	if (getArchitecture(hFile) != IMAGE_FILE_MACHINE_I386)
+	{
+		QMessageBox::warning(this, QString("Architecture not supported"), QString("PE Dissector only supports 32 bits application for now."));
+		statusBarLabel->setText(QString("Architecture of ") + QFileInfo(filename).fileName() + QString(" not supported."));
+		return false;
+	}
+	
+	PE_HEADERS32 peHeader32;
+	if (!readPEHeaders32(hFile, &peHeader32))
+	{
+		statusBarLabel->setText(QString("Error while parsing ") + QFileInfo(filename).fileName() + QString("!"));
+		return false;
+	}
+
+	statusBarLabel->setText(QFileInfo(filename).fileName() + QString(" successfully loaded"));
+
+	CloseHandle(hFile);
+	return true;
 }
--- PE Dissector/MainWindow.h
+++ PE Dissector/MainWindow.h
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <Windows.h>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/qlabel.h>
+#include <QtWidgets/qfiledialog.h>
+#include <QtWidgets/qmessagebox.h>
+#include <QtCore/qfile.h>
+#include <QtCore/qdebug.h>
 
 #include "ui_MainWindow.h"
+
+// As the PE Dissector API is written in C, we have to specify it to the compiler
+extern "C" {
+	typedef struct _PE_HEADERS32 PE_HEADERS32, *PPE_HEADERS32;
+	typedef unsigned __int64 QWORD;
+	BOOL isFileExecutable(HANDLE hFile);
+	WORD getArchitecture(HANDLE hFile);
+	BOOL readPEHeaders32(HANDLE hFile, PPE_HEADERS32 peHeader32);
+	WORD getSectionOfRVA(QWORD RVA, WORD numberOfSections, PIMAGE_SECTION_HEADER sectionHeaders);
+};
 #include "PE Dissector.h"
+
 
 class MainWindow : public QMainWindow
 {
@@ -13,7 +29,12 @@ class MainWindow : public QMainWindow
 public:
 	MainWindow(QWidget *parent = Q_NULLPTR);
 
+public slots:
+	void headerTree_selectionChanged();
+	void actionOpen_File_triggered();
+
 private:
 	Ui::MainWindowClass ui;
 	QLabel* statusBarLabel;
+	bool addFile(QString filename);
 };
